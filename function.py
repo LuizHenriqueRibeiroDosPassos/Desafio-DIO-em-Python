3f0f30ad036079ab48f125d4782bcb74e2ccfdc5
LuizHenriqueRibeiroDosPassos <luizhenriqueribeiro.15@gmail.com>
2024-01-04 20:18:37 -0300

    Minha calculadora de partidas rankeadas

---
+++ function.py
@@ -0,0 +1,6 @@
+def toast(bread,name = "Cliente"):
+    print("Torrada feita com " + pao)
+    print("Ela é um pedido de " + nome)
+
+toast("pâo de forma", "Luiz Henrique")  
+toast("pâo integral", "Isabela Lodi ")  
